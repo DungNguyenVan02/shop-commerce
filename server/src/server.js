1b4a65e747bcb60ba988d7f5996d07876efcb2b1
DungNguyenVan02 <nguyenvandung12112002@gmail.com>
2023-09-17 21:19:49 +0700

    create project

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", function (req, res) {
+	res.send("ok");
+});
+
+app.listen(port);
